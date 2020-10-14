519c56312ce4c3b3d265b41851c722a99c530b51
fiscn <fiscn2709@wrdsb.ca>
2020-10-14 19:59:48 +0000

    Finished A7 Q2 program

--- Main.java
+++ Main.java
@@ -1,15 +1,39 @@
+import java.util.Scanner; 
+
 /**
  *
- * @author 
+ * @author Neil Fischer
  */
-public class Main {
+public class Main { 
+
+  //Create factor method 
+  public static int factors(int factorNum){
+    //create factor formula 
+    for(int i = 1; i <= factorNum; i++){
+      if(factorNum % i == 0){
+        System.out.println(i);
+      }
+    } 
+    return factorNum;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Create Scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+    //prompt the user to enter in a number 
+    System.out.println("Enter a number to find the factors"); 
+
+    //put the users number into the variable 
+    int factorNum = input.nextInt(); 
+
+    int factorMethod = factors(factorNum); 
+
+
     
   }
 }
